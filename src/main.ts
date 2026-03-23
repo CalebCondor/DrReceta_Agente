d093c657272905565307d98c8841a0f01742fd17
Caleb Condor <calebcondor553@gmail.com>
2026-03-23 09:51:20 -0500

    feat: update CORS configuration to allow specific origins for enhanced security

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: ['http://localhost:3001', 'https://drreceta.com'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const port = process.env.PORT ?? 3000;
