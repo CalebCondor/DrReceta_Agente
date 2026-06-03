c3a3f3336cecaf262c8e954f76ba3e195eee7acd
Caleb Condor <calebcondor553@gmail.com>
2026-06-03 08:40:04 -0500

    feat: add global status URL for API integration

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -58,3 +58,6 @@ export const RESIDENTES_EDIT_PROFILE_URL =
 
 export const TURISTAS_EDIT_PROFILE_URL =
   'https://islandmedpr.com/apiia/api/turistas/edit_profile.php';
+
+export const STATUS_GLOBAL_URL =
+  'https://islandmedpr.com/apiia/api/status_by_code.php';
