9465a09de6fa8f94e8649786d80b6bb994b27200
Cristhofer-0 <136040301+Cristhofer-0@users.noreply.github.com>
2026-06-10 20:16:49 -0500

    Cambio de url

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,13 +1,14 @@
-export const LOGIN_URL = 'https://www.doctorrecetas.com/api/login.php';
+export const LOGIN_URL = 'https://api.doctorrecetas.com/api/login.php';
 export const VERIFICAR_REGISTRAR_URL =
-  'https://www.doctorrecetas.com/api/verificar_o_registrar_usuario.php';
-export const PERFIL_URL = 'https://doctorrecetas.com/api/perfil.php';
-export const MIS_ORDENES_URL = 'https://doctorrecetas.com/api/mis_ordenes.php';
-export const MIS_PAGOS_URL = 'https://doctorrecetas.com/api/mis_pagos.php';
+  'https://api.doctorrecetas.com/api/verificar_o_registrar_usuario.php';
+export const PERFIL_URL = 'https://api.doctorrecetas.com/api/perfil.php';
+export const MIS_ORDENES_URL =
+  'https://api.doctorrecetas.com/api/mis_ordenes.php';
+export const MIS_PAGOS_URL = 'https://api.doctorrecetas.com/api/mis_pagos.php';
 export const TODAS_LAS_ORDENES_URL =
-  'https://doctorrecetas.com/api/todas_las_ordenes.php';
+  'https://api.doctorrecetas.com/api/todas_las_ordenes.php';
 export const PRODUCTOS_BASE_URL = 'https://drreceta.com/es';
 export const CREAR_COMPRA_URL =
-  'https://www.doctorrecetas.com/api/crear_compra.php';
+  'https://api.doctorrecetas.com/api/crear_compra.php';
 export const VERIFICAR_CODIGO_URL =
-  'https://www.doctorrecetas.com/api/api/verificar_codigo.php';
+  'https://api.doctorrecetas.com/api/verificar_codigo.php';
