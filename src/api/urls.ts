d7d58cd8c699603bdbbed690a26f885e7ff29d4d
Caleb Condor <calebcondor553@gmail.com>
2026-08-12 17:03:14 -0500

    fix(urls): update API URLs to use the production server

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,19 +1,17 @@
-export const LOGIN_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/login.php';
+export const LOGIN_URL = 'http://api.tulicenciapr.com:2053/api/login.php';
 export const VERIFICAR_REGISTRAR_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/Ia/verificar-o-registrar-usuario';
-export const PERFIL_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/perfil.php';
+  'http://api.tulicenciapr.com:2053/api/Ia/verificar-o-registrar-usuario';
+export const PERFIL_URL = 'http://api.tulicenciapr.com:2053/api/perfil.php';
 export const MIS_ORDENES_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/mis_ordenes.php';
+  'http://api.tulicenciapr.com:2053/api/mis_ordenes.php';
 export const MIS_PAGOS_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/mis_pagos.php';
+  'http://api.tulicenciapr.com:2053/api/mis_pagos.php';
 export const TODOS_LOS_TRAMITES_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/Ia/listaTodosTramite';
+  'http://api.tulicenciapr.com:2053/api/Ia/listaTodosTramite';
 export const SELLOS_POR_TRAMITE_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/Sello/Tramite/{id}';
+  'http://api.tulicenciapr.com:2053/api/Sello/Tramite/{id}';
 export const PRODUCTOS_BASE_URL = 'https://drreceta.com/es';
 export const CREAR_COMPRA_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/Pago/inicioPagoIa';
+  'http://api.tulicenciapr.com:2053/api/Pago/inicioPagoIa';
 export const VERIFICAR_CODIGO_URL =
-  'https://graveyard-unroasted-steadying.ngrok-free.dev/api/Ia/verificar-codigo';
+  'http://api.tulicenciapr.com:2053/api/Ia/verificar-codigo';
