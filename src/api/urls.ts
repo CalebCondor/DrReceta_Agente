4ed90032267ab381cc2d5b0365a22f5dfa16db18
Caleb Condor <calebcondor553@gmail.com>
2026-08-13 08:20:17 -0500

    fix(urls): update API URLs to use HTTPS for improved security

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,17 +1,17 @@
-export const LOGIN_URL = 'http://api.tulicenciapr.com:2053/api/login.php';
+export const LOGIN_URL = 'https://api.tulicenciapr.com:2053/api/login.php';
 export const VERIFICAR_REGISTRAR_URL =
-  'http://api.tulicenciapr.com:2053/api/Ia/verificar-o-registrar-usuario';
-export const PERFIL_URL = 'http://api.tulicenciapr.com:2053/api/perfil.php';
+  'https://api.tulicenciapr.com:2053/api/Ia/verificar-o-registrar-usuario';
+export const PERFIL_URL = 'https://api.tulicenciapr.com:2053/api/perfil.php';
 export const MIS_ORDENES_URL =
-  'http://api.tulicenciapr.com:2053/api/mis_ordenes.php';
+  'https://api.tulicenciapr.com:2053/api/mis_ordenes.php';
 export const MIS_PAGOS_URL =
-  'http://api.tulicenciapr.com:2053/api/mis_pagos.php';
+  'https://api.tulicenciapr.com:2053/api/mis_pagos.php';
 export const TODOS_LOS_TRAMITES_URL =
-  'http://api.tulicenciapr.com:2053/api/Ia/listaTodosTramite';
+  'https://api.tulicenciapr.com:2053/api/Ia/listaTodosTramite';
 export const SELLOS_POR_TRAMITE_URL =
-  'http://api.tulicenciapr.com:2053/api/Sello/Tramite/{id}';
-export const PRODUCTOS_BASE_URL = 'https://drreceta.com/es';
+  'https://api.tulicenciapr.com:2053/api/Sello/Tramite/{id}';
+export const PRODUCTOS_BASE_URL = 'httpss://drreceta.com/es';
 export const CREAR_COMPRA_URL =
-  'http://api.tulicenciapr.com:2053/api/Pago/inicioPagoIa';
+  'https://api.tulicenciapr.com:2053/api/Pago/inicioPagoIa';
 export const VERIFICAR_CODIGO_URL =
-  'http://api.tulicenciapr.com:2053/api/Ia/verificar-codigo';
+  'https://api.tulicenciapr.com:2053/api/Ia/verificar-codigo';
